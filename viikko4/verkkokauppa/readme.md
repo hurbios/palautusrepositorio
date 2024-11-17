3a5917b52d1c66fbf117e18a4d2f87bd9ff7927a
hurbios <15719468+hurbios@users.noreply.github.com>
2024-11-17 23:46:53 +0200

    viikko4 verkkokauppa add more tests

---
+++ viikko4/verkkokauppa/readme.md
@@ -0,0 +1,3 @@
+# commands
+poetry run pytest
+poetry run coverage run --branch -m pytest src; poetry run coverage report -m
--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -94,3 +94,48 @@ class TestKauppa(unittest.TestCase):
         kauppa.tilimaksu("meitsi", "15")
         
         self.pankki_mock.tilisiirto.assert_called_with("meitsi", 42, "15", "33333-44455", 5)
+
+
+    def test_kaupan_metodi_aloita_asiointi_nollaa_kaupan_tilan(self):
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+            if tuote_id == 2:
+                return 0
+        
+        self.varasto_mock.saldo.side_effect = varasto_saldo
+
+        def generaattori():
+            for i in range(10):
+                yield i
+            return 10
+
+        viitegeneraattori_mock = Mock()
+        viitegeneraattori_mock.uusi.side_effect = generaattori()
+
+        kauppa = Kauppa(self.varasto_mock, self.pankki_mock, viitegeneraattori_mock)
+
+        # tehdään ostokset
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.lisaa_koriin(2)
+        kauppa.tilimaksu("meitsi", "15")
+        self.pankki_mock.tilisiirto.assert_called_with("meitsi", 0, "15", "33333-44455", 5)
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.lisaa_koriin(2)
+        kauppa.tilimaksu("meitsi", "15")
+        self.pankki_mock.tilisiirto.assert_called_with("meitsi", 1, "15", "33333-44455", 5)
+
+    def test_kaupan_metodi_poista_korista_poistaa_tuotteen_ostoskorista(self):
+        kauppa = Kauppa(self.varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)
+
+        # tehdään ostokset
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.lisaa_koriin(2)
+        kauppa.poista_korista(2)
+        kauppa.tilimaksu("meitsi", "15")
+        self.varasto_mock.palauta_varastoon.assert_called_with(Tuote(2, "piimä", 2))
+        self.pankki_mock.tilisiirto.assert_called_with("meitsi", 42, "15", "33333-44455", 5)
+        
