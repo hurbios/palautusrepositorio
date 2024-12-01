8e6c576edcdf3ff541423a9cab5b879ef6798451
hurbios <15719468+hurbios@users.noreply.github.com>
2024-12-01 18:35:22 +0200

    viikko6: add new matchers

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,21 +1,47 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
+    # matcher = And(
+    #     HasAtLeast(5, "goals"),
+    #     HasAtLeast(20, "assists"),
+    #     PlaysIn("PHI")
+    # )
+
+    # matcher = And(
+    #     Not(HasAtLeast(2, "goals")),
+    #     PlaysIn("NYR")
+    # )
+
+    # matcher = And(
+    #     HasFewerThan(2, "goals"),
+    #     PlaysIn("NYR")
+    # )
+
+    # matcher = Or(
+    #     HasAtLeast(45, "goals"),
+    #     HasAtLeast(70, "assists")
+    # )
+
     matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
     )
 
     for player in stats.matches(matcher):
         print(player)
 
+    # filtered_with_all = stats.matches(All())
+    # print(len(filtered_with_all))
 
 if __name__ == "__main__":
     main()
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -27,3 +27,42 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+
+class Not:
+    def __init__(self, matcher):
+        self._matcher = matcher
+
+    def test(self, player):
+        if not self._matcher.test(player):
+            return True
+
+        return False
+    
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
+
+class All:
+    def __init__(self):
+        pass
+
+    def test(self, _):
+        return True
+    
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
